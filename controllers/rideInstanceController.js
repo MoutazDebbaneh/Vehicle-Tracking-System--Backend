14a980754dc9f32bf14894ba69d04884e219a929
Moutaz Debbaneh <matazdev@gmail.com>
2022-08-25 18:05:49 +0300

    Bugfix

--- controllers/rideInstanceController.js
+++ controllers/rideInstanceController.js
@@ -107,7 +107,7 @@ exports.end = async function (req, res) {
     rideInstance.end_date = end_date;
     await rideInstance.save();
 
-    if (ride.is_repeatitive) ride.is_finished = true;
+    if (!ride.is_repeatitive) ride.is_finished = true;
 
     ride.is_active = false;
     await ride.save();
